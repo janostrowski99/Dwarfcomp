99211637e903f453a83c711017786adeb4957ee4
janostrowski99 <64968610+janostrowski99@users.noreply.github.com>
2023-12-30 19:50:46 +0100

    random
    implemented randomnes

--- mutation.cu
+++ mutation.cu
@@ -10,31 +10,63 @@
 #include <math.h>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
+#include <curand.h>
+#include <curand_kernel.h>
 
-__global__ void mutate(int *genome)
+__global__ void mutate(int *genome,int lengh,curandState *state)
 {
   size_t ixy = blockIdx.x*blockDim.x+threadIdx.x;
 
   //printf("gpu %d and %d \n", ixy, genome[ixy]);
-  printf("gpu %d\n", genome[ixy]);
-  genome[ixy]++;
 
+  int mutpose= (int) (curand_uniform(&state[ixy+2])*(lengh));
+
+
+  int muttype= (int) (curand_uniform(&state[ixy+2])*(3))+1;
+
+  //float test=curand_uniform(&state[ixy]+ixy)*(lengh);
+
+  genome[mutpose]=(genome[mutpose]+muttype)%4;
+
+  printf("gpu %d\n", mutpose);
+  //printf("gpu %f\n", test);
 }
+
+__global__ void grand(curandState *state,unsigned long seed)
+{
+    size_t ixy = blockIdx.x*blockDim.x+threadIdx.x;
+
+    curand_init(seed,(ixy),0,&state[ixy+2]);
+}
+
 
 int main()
 {
+  int mutationnum;
+  int lengh;
+  int day;
+  int *genome;
 
-    int lengh=3;
-    int *genome;
-    genome=(int*)malloc(lengh*sizeof(int));
+  int createmut=5;
+  curandState *state;
+
+
+
+
+  FILE* ifile = fopen ("genome.txt", "r");
+
+  fscanf (ifile, "%d", &mutationnum);
+  fscanf (ifile, "%d", &day);
+  fscanf (ifile, "%d", &lengh);
+
+
+  genome=(int*)malloc(lengh*sizeof(int));
+
 
     for(int i=0;i<lengh;i++)
     {
-      genome[i]=i+1;
+      fscanf (ifile, "%d", &genome[i]);
     }
-
-
-    printf("test %d\n", genome[2]);
 
     cudaSetDevice(0);
 
@@ -42,14 +74,16 @@ int main()
     int *gpm;
     cudaMalloc(&gpm,  lengh*sizeof(int));
 
-
+    cudaMalloc((void **)&state,(createmut+2)*sizeof(curandState));
     cudaMemcpy(gpm,genome,lengh*sizeof(int),cudaMemcpyHostToDevice);
-    mutate<<< lengh, 1 >>>(gpm);
+    grand<<< createmut, 1 >>>(state,unsigned(time(NULL)));
+    cudaDeviceSynchronize();
+    mutate<<< createmut, 1 >>>(gpm,lengh,state);
     cudaMemcpy(genome,gpm,lengh*sizeof(int),cudaMemcpyDeviceToHost);
 
 
 
-    printf("test %d\n", genome[2]);
+
 
 
 
