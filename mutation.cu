38fd2644af5b1c930a26528012a8931c056f46d1
janostrowski99 <64968610+janostrowski99@users.noreply.github.com>
2024-01-08 14:20:38 +0100

    entropy calculation
    not jet checked

--- mutation.cu
+++ mutation.cu
@@ -13,6 +13,7 @@
 #include <curand.h>
 #include <curand_kernel.h>
 
+
 __global__ void mutate(int *genome,int lengh,curandState *state)
 {
   size_t ixy = blockIdx.x*blockDim.x+threadIdx.x;
@@ -40,6 +41,46 @@ __global__ void grand(curandState *state,unsigned long seed)
 }
 
 
+__global__ void calculateEntropy(int *genome,int posi, int types, int len, double *entropy)
+{
+    size_t ixy = blockIdx.x*blockDim.x+threadIdx.x;
+
+    int total=0;
+    int multi;
+    for(int j=ixy;j<ixy+len;j++)
+    {
+      multi=1;
+      for(int i=0;i<j-ixy;i++)
+      {
+        multi=multi*types;
+      }
+      total=total+(genome[j]*multi); //entropy state numbers
+
+    }
+    entropy[total]++;
+}
+
+double calculateEntropy( int posi ,int* entab) // possibilities and array for entropy
+{
+  double sum=0;
+  for(int i =0;i<posi;i++)
+  {
+    sum=sum+entab[i];
+  }
+  double entropy=0;
+  for(int i =0;i<posi;i++)
+  {
+    //cout<<entropy;
+    if((double)entab[i]/sum)
+    {
+      entropy=entropy+(((double)entab[i]/sum)*(double)TMath::Log2((double)entab[i]/sum));
+    }
+
+  }
+  return -entropy;
+}
+
+
 int main()
 {
   int mutationnum;
@@ -47,7 +88,20 @@ int main()
   int day;
   int *genome;
 
-  int createmut=5;
+
+  //chengables
+  int createmut=5; //how many mutations to create
+  int len=1; //leng of chcked chain
+  int types=4; //number of type variables
+  //~chengables
+
+  int posi=types;
+  for(int i=1;i<len;i++)
+  {
+    posi=posi*types;
+  }
+
+
   curandState *state;
 
 
@@ -73,14 +127,28 @@ int main()
 
     int *gpm;
     cudaMalloc(&gpm,  lengh*sizeof(int));
+    cudaMemcpy(gpm,genome,lengh*sizeof(int),cudaMemcpyHostToDevice);
+
+    double *entropy;
+    double enbuffer[posi];
+    for(int i=0;i<posi;i++)
+    {
+      enbuffer[i]=0;
+    }
+    cudaMalloc(&entropy,  posi*sizeof(double));
+    cudaMemcpy(entropy,enbuffer,posi*sizeof(int),cudaMemcpyHostToDevice);
 
     cudaMalloc((void **)&state,(createmut+2)*sizeof(curandState));
-    cudaMemcpy(gpm,genome,lengh*sizeof(int),cudaMemcpyHostToDevice);
+
     grand<<< createmut, 1 >>>(state,unsigned(time(NULL)));
     cudaDeviceSynchronize();
     mutate<<< createmut, 1 >>>(gpm,lengh,state);
-    cudaMemcpy(genome,gpm,lengh*sizeof(int),cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    calculateEntropy<<< lengh+1-len, 1 >>>(gpm,posi,types,len,entropy);
+    cudaDeviceSynchronize();
 
+    cudaMemcpy(genome,gpm,lengh*sizeof(int),cudaMemcpyDeviceToHost);
+    cudaMemcpy(enbuffer,entropy,posi*sizeof(double),cudaMemcpyDeviceToHost);
 
 
 
